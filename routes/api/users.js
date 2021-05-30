1087ca66babad33dad5c1e644ebbbb6bc051227d
Toni K <50409778+utviklerToni@users.noreply.github.com>
2021-05-30 08:21:48 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,7 +7,8 @@ const { check, validationResult } = require('express-validator');
 // #name       > test route
 // #access     > public
 router.post('/', [
-  check('name', 'Name is necessary').not().isEmpty(), check('email', 'Please include a valid email').isEmail(),
+  check('name', 'Name is necessary').not().isEmpty(), 
+  check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password of minimum 6 characters').isLength({min: 6})
 ], (req, res) => {
 	const error = 
