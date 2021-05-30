da07c8dab92384bf339eaaad9f937e623d28e9eb
utviklerToni <h1detoni@protonmail.com>
2021-05-30 08:52:31 +0200

    routes added

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,12 +6,25 @@ const { check, validationResult } = require('express-validator');
 // #endpoint   > api/users
 // #name       > test route
 // #access     > public
-router.post('/', [
-  check('name', 'Name is necessary').not().isEmpty(), check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please enter a password of minimum 6 characters').isLength({min: 6})
-], (req, res) => {
-	const error = 
-	res.send('user route');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Name is necessary').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check(
+			'password',
+			'Please enter a password of minimum 6 characters'
+		).isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('user route');
+	}
+);
 
 module.exports = router;
