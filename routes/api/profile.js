1d1c762c71cd6abb5be431d3f273c6ad0a1c4408
utviklerToni <h1detoni@protonmail.com>
2021-05-31 18:17:04 +0200

    checking gpg again

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -99,7 +99,7 @@ router.post(
 			res.json(profile);
 		} catch (err) {
 			console.error(err.message);
-			res.status(500).send('Server Error');
+			res.status(500).send('Server error');
 		}
 	}
 );
