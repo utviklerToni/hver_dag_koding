bcb31504debd5374e035dec79039fc4110f45ca7
utviklerToni <h1de.toni@protonmail.com>
2021-06-02 20:21:34 +0200

    add & delete comments finished

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -163,4 +163,78 @@ router.put('/unlike/:id', auth, async (req, res) => {
 	}
 });
 
+// route 	=> api/posts/comment/:id
+// desc  	=> comment on a post
+// access => private
+router.post(
+	'/',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select('-password');
+			const post = await Post.findById(req.params.id);
+
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			};
+
+			post.comments.unshift(newComment);
+
+			await post.save();
+
+			res.json(post.comments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('server error');
+		}
+	}
+);
+
+// route => DELETE api/costs/comment/:id/:comment_id
+// desc  => Delete comment
+// access => private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// pull comment
+		const comment = post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		);
+
+		// checking if comment exists or not
+		if (!comment) {
+			return res.status(404).json({ msg: 'comment does not exist' });
+		}
+
+		// checking if user is the owner of the comment
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'user not authorized' });
+		}
+
+		// get remove index
+		const removeIndex = post.comments
+			.map((comment) => comment.user.toString())
+			.indexOf(req.user.id);
+
+		post.likes.splice(removeIndex);
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('server error');
+	}
+});
+
 module.exports = router;
