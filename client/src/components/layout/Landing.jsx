af8297d442a6279ca125366b028a289726a582a4
utviklerToni <h1de.toni@protonmail.com>
2021-06-26 19:01:16 +0200

    landing css

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import '../../App.css';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import "./Landing.css";
+import "../../App.css";
 
 const Landing = ({ isAuthenticated }) => {
-	if (isAuthenticated) {
-		return <Redirect to='/dashboard' />;
-	}
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
-	return (
-		<section className='landing'>
-			<div className='dark-overlay'>
-				<div className='landing-inner'>
-					<h1 className='xlarge'>NorsKokk</h1>
-					<p className='lead'>create your profile</p>
-					<div className='buttons'>
-						<Link to='/register' className='border-box pad-marg'>
-							sign up
-						</Link>
-						<Link to='/login' className='border-box pad-marg'>
-							login
-						</Link>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="greet_outer card">
+          <h1 className="landing_greet">Velkommen til NorsKokk</h1>
+          <p className="lead">create your profile to explore</p>
+          <p>or explore list of chefs</p>
+          <div className="buttons">
+            <Link to="/register" className="border-box pad-marg">
+              sign up
+            </Link>
+            <Link to="/login" className="border-box pad-marg">
+              login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 Landing.propTypes = {
-	isAuthenticated: PropTypes.bool,
+  isAuthenticated: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
-	isAuthenticated: state.auth.isAuthenticated,
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps)(Landing);
